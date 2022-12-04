dd71624e650dd93929812ad871011cc35e3c70a7
bcooperstl <coop999@gmail.com>
2022-12-03 20:49:37 -0600

    Added day 3 part 2

--- src/solutions/aoc_day_3.c
+++ src/solutions/aoc_day_3.c
@@ -58,6 +58,20 @@ static char find_dupe(day_3_rucksack_t * rucksack)
     return '\0';
 }
 
+static char find_badge(day_3_group_t * group)
+{
+    for (int i=0; i<strlen(group->rucksacks[0]); i++)
+    {
+        if ((strchr(group->rucksacks[1], group->rucksacks[0][i]) != NULL) &&
+            (strchr(group->rucksacks[2], group->rucksacks[0][i]) != NULL))
+        {
+            return group->rucksacks[0][i];
+        }
+    }
+    fprintf(stderr, "!!! No common character found between [%s] and [%s] and [%s]\n", group->rucksacks[0], group->rucksacks[1], group->rucksacks[2]);
+    return '\0';
+}
+
 void day_3_part_1(char * filename, extra_args_t * extra_args, char * result)
 {
     int sum = 0;
@@ -79,7 +93,6 @@ void day_3_part_1(char * filename, extra_args_t * extra_args, char * result)
     while (current != NULL)
     {
         line_number++;
-        // should have 1 token for each line of format [A X] where A is elf and X is me
         if (current->num_tokens == 1)
         {
             token_data_t * td = current->head_token;
@@ -108,6 +121,46 @@ void day_3_part_1(char * filename, extra_args_t * extra_args, char * result)
         current = current->next;
     }
 
+    snprintf(result, MAX_RESULT_LENGTH+1, "%ld", sum);
+    return;
+}
+
+void day_3_part_2(char * filename, extra_args_t * extra_args, char * result)
+{
+    int sum = 0;
+    file_data_t fd;
+    day_3_group_t group;
+    
+    // read in the input file; no delimiters
+    file_data_init(&fd);
+    file_data_read_file(&fd, filename, "", 0, '\0', '\0');
+    if (fd.num_lines == 0)
+    {
+        fprintf(stderr, "Error reading in data from %s\n", filename);
+        file_data_cleanup(&fd);
+        return;
+    }
+    
+    int line_number = 0;
+    line_data_t * current = fd.head_line;
+    while (current != NULL)
+    {
+        for (int i=0; i<3; i++)
+        {
+            memset(group.rucksacks[i], '\0', MAX_DAY_3_RUCKSACK_LENGTH+1);
+            token_data_t * td = current->head_token;
+            strncpy(group.rucksacks[i], td->token, MAX_DAY_3_RUCKSACK_LENGTH);
+            line_number++;
+            current = current->next;
+        }
+        char badge = find_badge(&group);
+        int curr = get_priority(badge);
+        sum += curr;
+#ifdef DEBUG_DAY_3
+        printf("Lines %d-%d has matching badge %c worth %d priority\n", line_number-2, line_number, badge, curr);
+#endif
+    }
+    
     snprintf(result, MAX_RESULT_LENGTH+1, "%ld", sum);
     return;
 }

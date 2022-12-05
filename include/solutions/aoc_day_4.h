d0e354acccb1555d78739806938df7c1e5f18e2b
bcooperstl <coop999@gmail.com>
2022-12-04 22:22:42 -0600

    Added day 4 part 2 solution; passes test

--- include/solutions/aoc_day_4.h
+++ include/solutions/aoc_day_4.h
@@ -4,6 +4,6 @@
 #include "constants.h"
 
 void day_4_part_1(char * filename, extra_args_t * extra_args, char * result);
-//void day_4_part_2(char * filename, extra_args_t * extra_args, char * result);
+void day_4_part_2(char * filename, extra_args_t * extra_args, char * result);
 
 #endif
--- src/solutions/aoc_solutions.c
+++ src/solutions/aoc_solutions.c
@@ -35,6 +35,7 @@ void initialize_solutions(aoc_solutions_t * solutions)
     solutions->solutions[DAY_INDEX(3)][PART_INDEX(1)] = day_3_part_1;
     solutions->solutions[DAY_INDEX(3)][PART_INDEX(2)] = day_3_part_2;
     solutions->solutions[DAY_INDEX(4)][PART_INDEX(1)] = day_4_part_1;
+    solutions->solutions[DAY_INDEX(4)][PART_INDEX(2)] = day_4_part_2;
 }
 
 int run_solution(aoc_solutions_t * solutions, int day, int part, char * filename, extra_args_t * extra_args, char * result)

b9a026762e88945e8ca7fc4cac7925d2f36ae5d7
Brian Cooper <coop999@gmail.com>
2023-05-25 17:17:19 +0100

    Finished day 20 part 1; change implmentation to remove node from list on multiple wrappings

--- src/solutions/aoc_day_20.c
+++ src/solutions/aoc_day_20.c
@@ -8,8 +8,11 @@
 
 static void move_ring_node(day_20_ring_node_t * ring_node)
 {
+    day_20_ring_node_t * old_prev = ring_node->prev;
+    day_20_ring_node_t * old_next = ring_node->next;
+    
 #ifdef DEBUG_DAY_20
-    printf("Working node %d %d **%d** %d %d\n", ring_node->prev->prev->value, ring_node->prev->value, ring_node->value, ring_node->next->value, ring_node->next->next->value);
+    printf("Working node %d %d %d**%d**%d %d %d\n", ring_node->prev->prev->value, ring_node->prev->value, ring_node->prev->next->value, ring_node->value, ring_node->next->prev->value, ring_node->next->value, ring_node->next->next->value);
 #endif
 
     if (ring_node->value == 0)
@@ -26,18 +29,19 @@ static void move_ring_node(day_20_ring_node_t * ring_node)
         
 #endif
         day_20_ring_node_t * insert_after_node = ring_node;
+        // pull it out of the list
+        old_prev->next = old_next;
+        old_next->prev = old_prev;
+        
         for (int i=0; i<ring_node->value; i++)
         {
             insert_after_node = insert_after_node->next;
         }
-        // remove ring_node from current spot in ring
-        ring_node->next->prev = ring_node->prev;
-        ring_node->prev->next = ring_node->next;
         
         // assign new prev and next for ring_node
         ring_node->prev = insert_after_node;
         ring_node->next = insert_after_node->next;
-        
+            
         // add ring_node in new spot
         ring_node->next->prev = ring_node;
         insert_after_node->next = ring_node;
@@ -50,13 +54,14 @@ static void move_ring_node(day_20_ring_node_t * ring_node)
         
 #endif
         day_20_ring_node_t * insert_before_node = ring_node;
+        // pull it out of the list
+        old_prev->next = old_next;
+        old_next->prev = old_prev;
+        
         for (int i=0; i>ring_node->value; i--)
         {
             insert_before_node = insert_before_node->prev;
         }
-        // remove ring_node from current spot in ring
-        ring_node->next->prev = ring_node->prev;
-        ring_node->prev->next = ring_node->next;
         
         // assign new prev and next for ring_node
         ring_node->prev = insert_before_node->prev;
@@ -68,7 +73,12 @@ static void move_ring_node(day_20_ring_node_t * ring_node)
     }
     
 #ifdef DEBUG_DAY_20
-    printf("After moving node %d %d **%d** %d %d\n", ring_node->prev->prev->value, ring_node->prev->value, ring_node->value, ring_node->next->value, ring_node->next->next->value);
+    printf("After moving node %d %d %d**%d**%d %d %d    old %d**%d %d**%d\n", 
+           ring_node->prev->prev->value, ring_node->prev->value, 
+           ring_node->prev->next->value, ring_node->value, ring_node->next->prev->value, 
+           ring_node->next->value, ring_node->next->next->value,
+           old_prev->value, old_next->prev->value,
+           old_prev->next->value, old_next->value);
 #endif
 }
 
@@ -114,10 +124,6 @@ static void read_and_parse_input(char * filename, day_20_index_t * index)
     {
         index->index_nodes[i].ring_node.next = &index->index_nodes[(i+1)%index->num_nodes].ring_node;
         index->index_nodes[i].ring_node.prev = &index->index_nodes[(index->num_nodes+i-1)%index->num_nodes].ring_node;
-#ifdef DEBUG_DAY_20
-        printf("Node %d has previous node %d and next node %d\n", i, (index->num_nodes+i-1)%index->num_nodes, (i+1)%index->num_nodes);
-#endif
-        
     }
     
     return;
@@ -137,6 +143,9 @@ void day_20_part_1(char * filename, extra_args_t * extra_args, char * result)
     }
     
     day_20_ring_node_t * coord = &index.index_nodes[index.zero_node_index].ring_node;
+#ifdef DEBUG_DAY_20
+    printf("Coordinate from position %d is %d\n", 0, coord->value);
+#endif
     for (int i=0; i<3; i++)
     {
         for (int j=0; j<1000; j++)

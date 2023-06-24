6525adf3625f8866641874c258465bc117430d07
Brian Cooper <coop999@gmail.com>
2023-06-24 02:44:49 +0100

    passes test case for day 25

---
+++ include/solutions/aoc_day_25.h
@@ -0,0 +1,9 @@
+#ifndef __AOC_DAY_25__
+#define __AOC_DAY_25__
+
+#include "constants.h"
+
+void day_25_part_1(char * forestname, extra_args_t * extra_args, char * result);
+//void day_25_part_2(char * forestname, extra_args_t * extra_args, char * result);
+
+#endif
